2e16fbc0cea0c64202a29eeafebf49ce8d51d082
fernando duenas <fernando39615@aim.com>
2021-02-24 21:12:03 -0500

    commit

--- react/react-intro/src/App-6.js
+++ react/react-intro/src/App-6.js
@@ -23,33 +23,25 @@ export default class App extends Component {
         {this.state.isLoading ? (
           <div>....Loading</div>
         ) : (
-            <ChildContainer
-              giphyArray={this.state.giphyArray}
-            />
+          <div className="container">
+            <div className="row">
+              {this.state.giphyArray.map((item) => {
+                return (
+                  <div className="col-sm-4" key={item.id}>
+                    <div className="card">
+                      <div className="card-body">
+                        <div>
+                          <img src={item.images.fixed_width_still.url} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         )}
       </div>
     );
   }
 }
-
-function ChildContainer(props) {
-  return (
-    <div className="container">
-      <div className="row">
-        {props.giphyArray.map((item) => {
-          return (
-            <div className="col-sm-4" key={item.id}>
-              <div className="card">
-                <div className="card-body">
-                  <div>
-                    <img src={item.images.fixed_width_still.url} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-        </div>
-      </div>
-  );
-};
