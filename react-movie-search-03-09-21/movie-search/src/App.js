ed14d810ea665eabc8d7ebcf565945d9f2416f69
fernando duenas <fernando39615@aim.com>
2021-03-09 19:15:29 -0500

    commit

---
+++ react-movie-search-03-09-21/movie-search/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Movie from './parts/Movie';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Movie />
+      </div>
+    )
+  }
+}
